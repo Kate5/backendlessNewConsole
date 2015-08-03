dd797a5e6bcbc0fcead56501a5b419b986b37e53
kateryna.maksimenko <kateryna.maksimenko@gmail.com>
2015-08-03 17:06:08 +0300

    support for grid connection

--- src/main/java/setup/SeleniumDriver.java
+++ src/main/java/setup/SeleniumDriver.java
@@ -1,9 +1,11 @@
 package main.java.setup;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.util.concurrent.TimeUnit;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by backendlessdev on 5/19/15.
@@ -13,15 +15,15 @@ public class SeleniumDriver
 
   static WebDriver driver;
 
-  public static WebDriver getDriver()
-  {
-    if( driver == null )
-    {
-      driver = new FirefoxDriver();
-    }
-    driver.manage().timeouts().implicitlyWait( 5, TimeUnit.SECONDS );
-    return driver;
-  }
+//  public static WebDriver getDriver()
+//  {
+//    if( driver == null )
+//    {
+//      driver = new FirefoxDriver();
+//    }
+//    driver.manage().timeouts().implicitlyWait( 5, TimeUnit.SECONDS );
+//    return driver;
+//  }
 
   // for selenium grid support
 
@@ -29,19 +31,19 @@ public class SeleniumDriver
   //java -jar selenium-server-standalone-2.45.0.jar -role hub
   //java -jar selenium-server-standalone-2.45.0.jar -role webdriver -hub http://localhost:4444/grid/register -port 5556
 
-//  public static WebDriver getDriver() {
-//    if (driver == null) {
-//      DesiredCapabilities capability = DesiredCapabilities.firefox();
-//      try {
-//        driver = new RemoteWebDriver(new URL("http://127.0.0.1:5556/wd/hub"), capability);
-//      } catch (MalformedURLException e) {
-//        e.printStackTrace();
-//      }
-//      capability.setBrowserName("firefox" );
-//      capability.setVersion("38.0.1");
-//    }
-//    return driver;
-//  }
+  public static WebDriver getDriver() {
+    if (driver == null) {
+      DesiredCapabilities capability = DesiredCapabilities.firefox();
+      try {
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+      } catch (MalformedURLException e) {
+        e.printStackTrace();
+      }
+      capability.setBrowserName("firefox" );
+      capability.setVersion("38.0.1");
+    }
+    return driver;
+  }
 
   public static void clearBrowserCache()
   {
